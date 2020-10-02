7c21be711ffc3ad77d85718cf8ce4dadb2836449
PXMAN <pxman18011997@gmail.com>
2020-10-02 17:00:58 +0700

    login page

---
+++ login-page/src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+
+  // constructor(
+  //   private formBuilder: FormBuilder
+  // ) {
+  //   this.loginForm = this.formBuilder.group({
+  //     name: '',
+  //     password: ''
+  //   });
+  // }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(data) {
+    this.loginForm.reset();
+  }
+
+}
